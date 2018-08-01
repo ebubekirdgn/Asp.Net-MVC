f15461c7bb4ab3d5a85674be35ee36c49fabb0b9
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-08-01 10:16:26 +0300

    Smtp Mail Service

---
+++ SentMail/SMTP/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using SMTP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace SMTP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMail(SMTPModel sMTPModel)
+        {
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com"; //Gmail Smtp Server
+                WebMail.SmtpPort = 587; //Gmail Port Numarası
+                WebMail.SmtpUseDefaultCredentials = true;
+                //sending emails with secure protocol
+                WebMail.EnableSsl = true; // Korumalı protokol ile mail gönderme aktifleştiriyoruz.
+                //EmailId used to send emails from application
+                WebMail.UserName = "ebubekir.dogan@bil.omu.edu.tr";
+                WebMail.Password = "**********";
+
+                WebMail.From = "SenderGamilId@gmail.com"; //Gönderen e-posta adresi.
+
+                WebMail.Send(to: sMTPModel.ToEmail, subject: sMTPModel.EmailSubject, body: sMTPModel.EMailBody, cc: sMTPModel.EmailCC, bcc: sMTPModel.EmailBCC, isBodyHtml: true); //Mail Gönderme
+                ViewBag.Status = "Email başarılı bir şekilde gönderildi..";
+            }
+            catch (Exception)
+            {
+                ViewBag.Status = "Mail gönderilemedi.Lütfen daha sonra tekrar deneyiniz.";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SentMail/SMTP/Models/SMTPModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SMTP.Models
+{
+    public class SMTPModel
+    {
+        [DataType(DataType.EmailAddress), Display(Name = "Alıcı Mail Adresi :")]
+        [Required]
+        public string ToEmail { get; set; }
+
+        [Display(Name = "Mesajınız :")]
+        [DataType(DataType.MultilineText)]
+        public string EMailBody { get; set; }
+
+        [Display(Name = "Konu :")]
+        public string EmailSubject { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "CC")]
+        public string EmailCC { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Gönderici Mail Adres")]
+        public string EmailBCC { get; set; }
+    }
+}
